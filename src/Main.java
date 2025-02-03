039b14a020ada1c3a7a93e7cbf13dfdc0841e1e9
Alescu <albert.escudero@gmail.com>
2025-02-03 19:14:09 +0100

    Exercici 8

--- src/Main.java
+++ src/Main.java
@@ -18,13 +18,32 @@ public class Main {
                 (p1,p2) -> p1.toString().compareToIgnoreCase(p2.toString()));
         System.out.println(llistatNombresCadenes);
 
+        Comparator<Object> objComparator = Comparator. comparing(Object::toString);
+        Collections.sort(llistatNombresCadenes, objComparator);
+        System.out.println(llistatNombresCadenes);
+
         // Exercici 7
         // Amb la llista de l’exercici anterior, ara ordena-la al revés, de cadena més llarga a més curta.
-        /*
+        Comparator<String> objComparator2 = Comparator. comparing(String::length);
         Collections.sort(llistatNombresCadenes,
-                (p1,p2) -> String.valueOf(p1).length()<String.valueOf(p2).length() ;
+                (p1,p2) -> p2.toString().length()-p1.toString().length());
         System.out.println(llistatNombresCadenes);
-        */
+
+        // Exercici 8
+        // Crea una Functional Interface que contingui un mètode anomenat reverse().
+        // Aquest mètode ha de rebre i ha de retornar un String. En el main() de la classe principal,
+        // injecta a la interfície creada mitjançant una lambda, el cos del mètode, de manera que torni
+        // la mateixa cadena que rep com a paràmetre però al revés. Invoca la instància de la interfície
+        // passant-li una cadena i comprova si el resultat és correcte.
+
+        Reverser strInv = (String _str) -> {
+            String str = _str;
+            StringBuffer strb = new StringBuffer();
+            for (int i = str.length(); i <= 0; i--) {
+                strb.append()[str.i];
+            }
+            ;
+        }
 
     }
 }
---
+++ src/Reverser.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+interface Reverser {
+    String reverse();
+}
